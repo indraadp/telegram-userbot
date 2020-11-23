41eef9bedfb1e8328867a9e5d5129444049813fe
Indra DP <53829264+indraadp@users.noreply.github.com>
2020-11-23 12:34:41 +0700

    module: afk.py: Change language to id ...
    Change language to Indonesian.

--- userbot/modules/afk.py
+++ userbot/modules/afk.py
@@ -25,29 +25,7 @@ from userbot import (  # noqa pylint: disable=unused-import isort:skip
 
 # ========================= CONSTANTS ============================
 AFKSTR = [
-    "I'm busy right now. Please talk in a bag and when I come back you can just give me the bag!",
-    "I'm away right now. If you need anything, leave a message after the beep:\n`beeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeep`!",
-    "You missed me, next time aim better.",
-    "I'll be back in a few minutes and if I'm not...,\nwait longer.",
-    "I'm not here right now, so I'm probably somewhere else.",
-    "Roses are red,\nViolets are blue,\nLeave me a message,\nAnd I'll get back to you.",
-    "Sometimes the best things in life are worth waiting for…\nI'll be right back.",
-    "I'll be right back,\nbut if I'm not right back,\nI'll be back later.",
-    "If you haven't figured it out already,\nI'm not here.",
-    "Hello, welcome to my away message, how may I ignore you today?",
-    "I'm away over 7 seas and 7 countries,\n7 waters and 7 continents,\n7 mountains and 7 hills,\n7 plains and 7 mounds,\n7 pools and 7 lakes,\n7 springs and 7 meadows,\n7 cities and 7 neighborhoods,\n7 blocks and 7 houses...\n\nWhere not even your messages can reach me!",
-    "I'm away from the keyboard at the moment, but if you'll scream loud enough at your screen, I might just hear you.",
-    "I went that way\n---->",
-    "I went this way\n<----",
-    "Please leave a message and make me feel even more important than I already am.",
-    "I am not here so stop writing to me,\nor else you will find yourself with a screen full of your own messages.",
-    "If I were here,\nI'd tell you where I am.\n\nBut I'm not,\nso ask me when I return...",
-    "I am away!\nI don't know when I'll be back!\nHopefully a few minutes from now!",
-    "I'm not available right now so please leave your name, number, and address and I will stalk you later.",
-    "Sorry, I'm not here right now.\nFeel free to talk to my userbot as long as you like.\nI'll get back to you later.",
-    "I bet you were expecting an away message!",
-    "Life is so short, there are so many things to do...\nI'm away doing one of them..",
-    "I am not here right now...\nbut if I was...\n\nwouldn't that be awesome?",
+    "`Sorry, Indra sedang offline karena sibuk.`",
 ]
 USER_AFK = {}
 afk_time = None
@@ -74,13 +52,13 @@ async def set_afk(afk_e):
     if string:
         AFKREASON = string
         await afk_e.edit(
-            f"Going AFK!\
-        \nReason: `{string}`"
-        )
+            f"`Sedang offline`\
+           \n`karena` `{string}`"
+            )
     else:
-        await afk_e.edit("Going AFK!")
+        await afk_e.edit("`Offline`")
     if BOTLOG:
-        await afk_e.client.send_message(BOTLOG_CHATID, "#AFK\nYou went AFK!")
+        await afk_e.client.send_message(BOTLOG_CHATID, "#AFK\n`Kamu offline`")
     ISAFK = True
     afk_time = datetime.now()  # pylint:disable=E0602
     raise StopPropagation
@@ -100,9 +78,6 @@ async def type_afk_is_not_true(notafk):
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
         ISAFK = False
-        msg = await notafk.respond("I'm no longer AFK.")
-        time.sleep(3)
-        await msg.delete()
         if BOTLOG:
             await notafk.client.send_message(
                 BOTLOG_CHATID,
@@ -143,7 +118,7 @@ async def mention_afk(mention):
     global afk_end
     back_alivee = datetime.now()
     afk_end = back_alivee.replace(microsecond=0)
-    afk_since = "a while ago"
+    afk_since = "`belum lama ini`"
     if mention.message.mentioned and not (await mention.get_sender()).bot and ISAFK:
         now = datetime.now()
         datime_since_afk = now - afk_time  # pylint:disable=E0602
@@ -156,7 +131,7 @@ async def mention_afk(mention):
         time %= 60
         seconds = time
         if days == 1:
-            afk_since = "Yesterday"
+            afk_since = "`kemarin`"
         elif days > 1:
             if days > 6:
                 date = now + datetime.timedelta(
@@ -167,16 +142,16 @@ async def mention_afk(mention):
                 wday = now + datetime.timedelta(days=-days)
                 afk_since = wday.strftime("%A")
         elif hours > 1:
-            afk_since = f"`{int(hours)}h{int(minutes)}m` ago"
+            afk_since = f"`{int(hours)} jam {int(minutes)} menit` `yang lalu`"
         elif minutes > 0:
-            afk_since = f"`{int(minutes)}m{int(seconds)}s` ago"
+            afk_since = f"`{int(minutes)} menit {int(seconds)} detik` `yang lalu`"
         else:
-            afk_since = f"`{int(seconds)}s` ago"
+            afk_since = f"`{int(seconds)} detik` `yang lalu`"
         if mention.sender_id not in USERS:
             if AFKREASON:
                 await mention.reply(
-                    f"I'm AFK since {afk_since}.\
-                        \nReason: `{AFKREASON}`"
+                    f"`Indra sedang offline sejak` {afk_since}.\
+                        \n`karena` `{AFKREASON}`"
                 )
             else:
                 await mention.reply(str(choice(AFKSTR)))
@@ -185,8 +160,8 @@ async def mention_afk(mention):
             if USERS[mention.sender_id] % randint(2, 4) == 0:
                 if AFKREASON:
                     await mention.reply(
-                        f"I'm still AFK since {afk_since}.\
-                            \nReason: `{AFKREASON}`"
+                        f"`Indra masih offline sejak` {afk_since}.\
+                            \n`karena` `{AFKREASON}`"
                     )
                 else:
                     await mention.reply(str(choice(AFKSTR)))
@@ -208,7 +183,7 @@ async def afk_on_pm(sender):
     global afk_end
     back_alivee = datetime.now()
     afk_end = back_alivee.replace(microsecond=0)
-    afk_since = "a while ago"
+    afk_since = "`belum lama ini`"
     if (
         sender.is_private
         and sender.sender_id != 777000
@@ -235,7 +210,7 @@ async def afk_on_pm(sender):
             time %= 60
             seconds = time
             if days == 1:
-                afk_since = "Yesterday"
+                afk_since = "`kemarin`"
             elif days > 1:
                 if days > 6:
                     date = now + datetime.timedelta(
@@ -246,16 +221,16 @@ async def afk_on_pm(sender):
                     wday = now + datetime.timedelta(days=-days)
                     afk_since = wday.strftime("%A")
             elif hours > 1:
-                afk_since = f"`{int(hours)}h{int(minutes)}m` ago"
+                afk_since = f"`{int(hours)} jam {int(minutes)} menit` `yang lalu`"
             elif minutes > 0:
-                afk_since = f"`{int(minutes)}m{int(seconds)}s` ago"
+                afk_since = f"`{int(minutes)} menit {int(seconds)} detik` `yang lalu`"
             else:
-                afk_since = f"`{int(seconds)}s` ago"
+                afk_since = f"`{int(seconds)} detik` `yang lalu`"
             if sender.sender_id not in USERS:
                 if AFKREASON:
                     await sender.reply(
-                        f"I'm AFK since {afk_since}.\
-                        \nReason: `{AFKREASON}`"
+                        f"`Indra sedang offline sejak` {afk_since}.\
+                        \n`karena` `{AFKREASON}`"
                     )
                 else:
                     await sender.reply(str(choice(AFKSTR)))
@@ -265,8 +240,8 @@ async def afk_on_pm(sender):
                 if USERS[sender.sender_id] % randint(2, 4) == 0:
                     if AFKREASON:
                         await sender.reply(
-                            f"I'm still AFK since {afk_since}.\
-                            \nReason: `{AFKREASON}`"
+                            f"`Indra masih offline sejak` {afk_since}.\
+                            \n`karena` `{AFKREASON}`"
                         )
                     else:
                         await sender.reply(str(choice(AFKSTR)))
